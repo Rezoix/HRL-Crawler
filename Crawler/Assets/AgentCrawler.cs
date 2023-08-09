a818b4f4eec27275f12d6bce9dca23bfb9ec1c00
Rezoix <sakuos@hotmail.fi>
2023-08-09 16:34:29 +0300

    Intial commit

---
+++ Crawler/Assets/AgentCrawler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class AgentCrawler : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+       
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+
+
+
+}
